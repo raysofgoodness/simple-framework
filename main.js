6ff0d4ca65256070880054d161f58f8ec867d0a7
raysofgoodness <shady5324@gmail.com>
2023-01-24 10:43:49 +0200

    corrected reactivity

--- main.js
+++ main.js
@@ -9,4 +9,11 @@ const nodeEl = node('div', {class: 'container mx-auto'},
 
 mount(nodeEl, document.querySelector('#app'));
 
+const state = reactive({
+    description: 'This is test description, that show reactivity',
+});
 
+
+watchEffect(() => {
+    console.log(state.description);
+});
