45b6038effb9e10f19018357f644a92dca807428
raysofgoodness <shady5324@gmail.com>
2023-01-24 10:07:19 +0200

    added reactive example

---
+++ reactivity.js
@@ -0,0 +1,44 @@
+let activeEffect;
+
+const watchEffect = (fn) => {
+    activeEffect = fn;
+    fn();
+    activeEffect = null;
+};
+
+class Dependency{
+    constructor() {
+        this.subscribes = new Set();
+    }
+
+    depend() {
+        if (activeEffect) this.subscribes.add(activeEffect);
+    }
+
+    notify() {
+        this.subscribes.forEach(subscriber => subscriber());
+    }
+}
+
+const reactive = (obj) => {
+    Object.keys(obj).forEach(key => {
+       const dep = new Dependency();
+       let value = obj[key];
+
+       Object.defineProperty(obj, key, {
+          get() {
+              dep.depend()
+              return value;
+          },
+
+          set(newValue) {
+            if (newValue !== value) {
+                value = newValue;
+                dep.notify();
+            }
+          }
+       });
+    });
+
+    return obj;
+};
