f64df5cf47d2ded8301bd8a610c0830870b7a76e
raysofgoodness <shady5324@gmail.com>
2023-01-25 11:08:10 +0200

    all build using reactivity data

--- main.js
+++ main.js
@@ -1,19 +1,30 @@
-const nodeEl = node('div', {class: 'container mx-auto'},
-    [
-        node('img', {class: 'w-64 mt-6 mx-auto mb-6' , src: 'img/logo-sm.jpg', alt: 'logo'}, []),
-        node('h1', {class: 'mb-4 text-yellow-700 text-4xl text-center'}, 'Mini Pet JavaScript ' +
-            'Framework'),
-        node('p', {class: 'text-blue-500 text-xl text-center'}, 'Here small and lightweight ' +
-            'JavaScript Framework'),
-    ]);
-
-mount(nodeEl, document.querySelector('#app'));
-
 const state = reactive({
-    description: 'This is test description, that show reactivity',
+    value: '',
 });
 
+function render(text) {
+    return node('div', {class: 'container mx-auto text-center'},
+        [
+            node('img', {class: 'w-64 mt-6 mx-auto mb-6' , src: 'img/logo-sm.jpg', alt: 'logo'}, []),
+            node('h1', {class: 'mb-4 text-yellow-700 text-4xl text-center'}, 'Mini Pet JavaScript ' +
+                'Framework'),
+            node('p', {class: 'mb-4 text-blue-500 text-xl text-center'}, text),
+            node('input', {class: 'mx-auto bg-slate-500 text-slate-50', oninput: 'state.value = this.value'}, []),
+        ]);
+}
+
+
+// mount(nodeEl, document.querySelector('#app'));
+
+let currentNode;
 
 watchEffect(() => {
-    console.log(state.description);
+    if(!currentNode) {
+        currentNode = render(state.value);
+        mount(currentNode, document.querySelector('#app'));
+    } else {
+        const newNode = render(state.value);
+        patch(currentNode, newNode);
+        currentNode = newNode;
+    }
 });
